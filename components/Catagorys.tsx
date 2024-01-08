297269c69ff107d6c5433f8ad852fc63ef59fe0c
suborno180 <64451255+suborno180@users.noreply.github.com>
2024-01-08 22:25:59 +0600

    update

--- components/Catagorys.tsx
+++ components/Catagorys.tsx
@@ -21,7 +21,7 @@ export const CategoryPage = () => {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {categories.map((category, index) => (
                     <Link href={`/categories/${category.toLowerCase()}`} key={index}>
-                        <div className="bg-white p-4 rounded-md shadow-md transition duration-300 transform hover:scale-105 hover:bg-orange-500 hover:text-white transition-all">
+                        <div className="bg-white p-4 rounded-md shadow-md duration-300 transform hover:scale-105 hover:bg-orange-500 hover:text-white transition-all">
                             <h2 className="text-lg font-semibold">{category}</h2>
                         </div>
                     </Link>
