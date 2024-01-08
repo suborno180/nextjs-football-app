320954fc3f4cb2bb0873b18d7135c0a5187c38e3
suborno180 <64451255+suborno180@users.noreply.github.com>
2024-01-08 14:00:09 +0600

    update

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Loading = () => {
     return (
-        <div className='fixed top-0 left-0 w-screen h-screen bg-slate-500'>
+        <div className='fixed top-0 left-0 w-screen h-screen bg-gray-800'>
             <div className='w-full h-screen grid place-content-center'>
-                <span className="loading loading-ring loading-lg scale-125"></span>
+                <span className="loading loading-ring loading-lg scale-125 bg-slate-100"></span>
             </div>
         </div>
     )
