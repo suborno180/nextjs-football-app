2b425bc35f740bbbd8a90ead75ad7141219948c2
suborno180 <64451255+suborno180@users.noreply.github.com>
2024-01-08 22:18:54 +0600

    update

--- components/navbar/mobileNavbar.tsx
+++ components/navbar/mobileNavbar.tsx
@@ -9,7 +9,7 @@ export const MobileNavbar = () => {
                 <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content">
                     {/* Page content here */}
-                    <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary">
+                    <label htmlFor="my-drawer-4" className="drawer-button btn btn-ghost">
                         <GiHamburgerMenu size={20} />
                     </label>
                 </div>
