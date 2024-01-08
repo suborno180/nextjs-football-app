984a1754888268d0f8064892f06ea6c139b45511
suborno180 <64451255+suborno180@users.noreply.github.com>
2024-01-08 11:46:43 +0600

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body className={`${inter.className} min-h-screen`}>
         <ProgressLoding>
           {children}
