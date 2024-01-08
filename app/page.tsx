645730d4df4b6c3f1c574b9bc16795385d4c00fc
suborno180 <64451255+suborno180@users.noreply.github.com>
2024-01-08 22:33:19 +0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
           <div className="container mx-auto max-w-[1000px]">
             <div className='grid lg:grid-cols-7 w-full'>
               <div className='w-full min-w-[260px] min-h-36 lg:min-h-screen lg:col-span-2 bg-gray-200 lg:order-1'>
-                <div className='w-full h-full grid place-content-start lg:place-content-center'>
+                <div className='w-full h-full grid place-content-center lg:place-content-start'>
                   <h1 className='bg-green-500'>Google ads</h1>
                 </div>
               </div>
--- components/Catagorys.tsx
+++ components/Catagorys.tsx
@@ -11,6 +11,7 @@ const categories = [
     'NFL',
     'Tennis',
     'Volleyball',
+    'wwe'
 ];
 
 export const CategoryPage = () => {
