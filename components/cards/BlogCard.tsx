22317dcfc7ddbb41c1c5010f216bd9c45a66e062
suborno180 <64451255+suborno180@users.noreply.github.com>
2024-01-16 23:00:50 +0600

    update

--- components/cards/BlogCard.tsx
+++ components/cards/BlogCard.tsx
@@ -13,7 +13,7 @@ export const BlogCard: React.FC<BlogCardProps> = ({ title, date, image, alt }) =
     <>
       <div className="card p-4 hover:bg-gray-100 transition-all rounded-md">
         <div className="image-container relative h-48 overflow-hidden rounded-md mb-4">
-          <Image src={image} alt={alt} layout="fill" objectFit="cover" loading='lazy' />
+          <Image src={image??'/image-placeholder.png'} alt={alt} layout="fill" objectFit="cover" loading='lazy' />
         </div>
         <h2 className="text-xl font-bold text-black">{title}</h2>
         <p className="text-gray-500">{date}</p>
