a6ab0e2df46996d93377b908a22ccff0082876a0
suborno180 <64451255+suborno180@users.noreply.github.com>
2024-01-16 15:32:05 +0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -18,35 +18,34 @@ export default function Home() {
       <main className='min-h-screen'>
         {/* Hero section */}
         <Navbar />
-        <div className="hero min-h-96 bg-base-200">
+        {/* <div className="hero min-h-96 bg-base-200">
           <div className="hero-content text-center">
             <div className="max-w-full">
-              {/* Google Ads Section (Hero) */}
               <div className="w-full min-h-36 lg:h-full ">
-                {/* Replace the following line with your actual Hero ad code */}
+                Replace the following line with your actual Hero ad code
                 Google Ads (Hero Placeholder)
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
         <section>
           <div className="container mx-auto max-w-[1000px]">
             <div className='grid lg:grid-cols-7 w-full'>
               {/* Sidebar Section */}
-              <div className='w-full min-w-[260px] min-h-36 lg:min-h-screen lg:col-span-2 bg-gray-100 lg:order-1'>
-                {/* Google Ads Section (Sidebar) */}
+              {/* <div className='w-full min-w-[260px] min-h-36 lg:min-h-screen lg:col-span-2 bg-gray-100 lg:order-1'>
+                Google Ads Section (Sidebar)
                 <div className='w-full h-full '>
-                  {/* Replace the following line with your actual Sidebar ad code */}
+                  Replace the following line with your actual Sidebar ad code
                   Google Ads (Sidebar Placeholder)
                 </div>
-              </div>
+              </div> */}
               {/* Main Content Section */}
               <div className='w-full min-h-screen lg:col-span-5'>
                 <div className='w-full min-h-10 my-8 px-4'>
                   <CategoryPage />
                 </div>
                 <div className='px-4'>
-                  <h1 className="text-4xl font-bold mb-4 capitalize">match schedule </h1>
+                  {/* <h1 className="text-4xl font-bold mb-4 capitalize">match schedule </h1> */}
                   <Card />
                 </div>
               </div>
