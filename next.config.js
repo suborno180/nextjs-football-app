6af25789906061d448002b78d6ce27836fd2354c
suborno180 <64451255+suborno180@users.noreply.github.com>
2024-01-18 17:40:18 +0600

    update

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
         // See: https://www.npmjs.com/package/next-sitemap
         exclude: ['/terms', '/privacy'], // Add any paths you want to exclude from the sitemap
     },
-    reactStrictMode: true,
+    reactStrictMode: false,
 }
 
 module.exports = nextConfig
